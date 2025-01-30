8580b64dada16f0fe8b28308df0d4033c8d2b127
avonamolos <solomanovarjk@gmail.com>
2025-01-30 06:46:59 +0100

    practiced implementing the QuasiStack data structure

--- algorithms/src/stacks/QuasiStack.java
+++ algorithms/src/stacks/QuasiStack.java
@@ -1,79 +1,79 @@
-package stacks;
-
-import com.finki.DLLNode;
-import com.finki.Stack;
-
-import java.util.NoSuchElementException;
-
-class QuasiStack<E extends Comparable<E>> implements Stack<E> {
-    private DLLNode<E> top, bottom;
-
-    QuasiStack() {
-        top = null;
-        bottom = null;
-    }
-
-    public boolean isEmpty() {
-        return top == null;
-    }
-
-    @Override
-    public E peek() {
-        return null;
-    }
-
-    public E peekTop() {
-        if (top == null) {
-            throw new NoSuchElementException();
-        }
-        return top.element;
-    }
-
-    public E peekBottom() {
-        if (bottom == null) {
-            throw new NoSuchElementException();
-        }
-        return bottom.element;
-    }
-
-    public void clear() {
-        top = null;
-        bottom = null;
-    }
-
-    public void push(E e) {
-        DLLNode<E> ins = new DLLNode<E>(e, null, top);
-        if (top == null)
-            bottom = ins;
-
-        else
-            top.pred = ins;
-        top = ins;
-    }
-
-    public E pop() {
-        if (top == null)
-            throw new NoSuchElementException();
-
-        E topElem = top.element; // vidi koj e elementot pred da go ubies
-        E bottomElem = bottom.element;
-
-        if (top == bottom) {
-            top = null;
-            bottom = null;
-            return topElem;
-        }
-
-        if (topElem.compareTo(bottomElem) < 0) {
-            bottom = bottom.pred;
-            bottom.succ = null;
-            return bottomElem;
-        }
-
-        top = top.succ;
-        top.pred = null;
-        return topElem;
-
-    }
-
-}
+//package stacks;
+//
+//import com.finki.DLLNode;
+//import com.finki.Stack;
+//
+//import java.util.NoSuchElementException;
+//
+//class QuasiStack<E extends Comparable<E>> extends Stack<E> {
+//    private DLLNode top, bottom;
+//
+//    QuasiStack() {
+//        top = null;
+//        bottom = null;
+//    }
+//
+//    public boolean isEmpty() {
+//        return top == null;
+//    }
+//
+//    @Override
+//    public E peek() {
+//        return null;
+//    }
+//
+//    public E peekTop() {
+//        if (top == null) {
+//            throw new NoSuchElementException();
+//        }
+//        return top.element;
+//    }
+//
+//    public E peekBottom() {
+//        if (bottom == null) {
+//            throw new NoSuchElementException();
+//        }
+//        return bottom.element;
+//    }
+//
+//    public void clear() {
+//        top = null;
+//        bottom = null;
+//    }
+//
+//    public void push(E e) {
+//        DLLNode<E> ins = new DLLNode<E>(e, null, top);
+//        if (top == null)
+//            bottom = ins;
+//
+//        else
+//            top.pred = ins;
+//        top = ins;
+//    }
+//
+//    public E pop() {
+//        if (top == null)
+//            throw new NoSuchElementException();
+//
+//        E topElem = top.element; // vidi koj e elementot pred da go ubies
+//        E bottomElem = bottom.element;
+//
+//        if (top == bottom) {
+//            top = null;
+//            bottom = null;
+//            return topElem;
+//        }
+//
+//        if (topElem.compareTo(bottomElem) < 0) {
+//            bottom = bottom.pred;
+//            bottom.succ = null;
+//            return bottomElem;
+//        }
+//
+//        top = top.succ;
+//        top.pred = null;
+//        return topElem;
+//
+//    }
+//
+//}
